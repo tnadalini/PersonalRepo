34c256679608b77809f4f3b72b68b85212075782
tnadalini <63377908+tnadalini@users.noreply.github.com>
2022-03-16 13:28:28 -0700

    +some axios functionality added -not working properly, but error is thrown as expected upon axios attempt

--- PokeAPIProject/PokeAPIFunctions.js
+++ PokeAPIProject/PokeAPIFunctions.js
@@ -1,37 +1,30 @@
-// JavaScript source code
-function getPokemon() {
-    //needs a lot of editing
-    //let link = 'www.google.com';
-} axios(
+function setPokemonURL() {
+    const input = new String(document.getElementById("pokemon").value);
+    //document.write(document.getElementById("pokemon").value);
+    const url = "https://pokeapi.co/api/v2/pokemon/".concat(input.trim(),"/");
+    getPokeData(url);
+}
+
+function getPokeData(daURL) {
+    //document.write(daURL);
+
+    //axios call using the made URL
+    axios(
     {
         method: 'get',
-        url: 'https://v2.jokeapi.dev/joke/Programming'
-    }
-)
-    .then(function (response) {
-        let type = response.data.type;
-        let placement = document.querySelector(".Joke");
-        let ResultLocation = document.querySelector(".Result");
-        ResultLocation.style.visibility = 'hidden'
-        if (type.includes("single", 0)) {
-            let joke = response.data.joke
-            placement.innerText = joke;
-        }
-        else if (type.includes("twopart", 0)) {
-            let setup = response.data.setup;
-            let delivery = response.data.delivery;
-            placement.innerText = setup;
-            let ResultLocation = document.querySelector(".Result");
-            ResultLocation.style.visibility = 'hidden'
-            ResultLocation.innerText = delivery;
-            setTimeout(JokeResult, 4000);
-        }
-        else {
-            placement.innerText = "try again in a few moments";
-        }
+        url: daURL
     }
     )
-    .catch(function (error) {
-        console.log("error: try again in a few moments.");
+    .then(function (response) {
+        var id = response.data.id;
+        var name = response.data.name;
+        //var types = response.data.types;
+        alert(new String(id.concat(",",name)));
     }
-    );
+    )
+        .catch(function (error) {
+            console.log("Error during AXIOS call! Please try again later.");
+            alert("Error during AXIOS call! Please make sure you ensure your input was valid");
+        })
+
+    }
